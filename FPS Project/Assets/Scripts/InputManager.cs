5a93a7010e93471e9add0685c113062fbd81a609
Kyle Miranda <miranda14355k@gmail.com>
2021-08-03 15:52:27 -0400

    Menu + Movement re-work
    Added main menu. Reworked movement system. Still need to add jump mechanic.

--- FPS Project/Assets/Scripts/InputManager.cs
+++ FPS Project/Assets/Scripts/InputManager.cs
@@ -1,18 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
 
-public class InputManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public class InputManager : MonoBehaviour {
+    
+    public Movement movement;
+    public MouseLook mouseLook;
+    
+    PlayerControls controls;
+    PlayerControls.GroundMovementActions groundMovement;
+
+    Vector2 horizontalInput;
+    Vector2 mouseInput;
+    
+    private void Awake ()
     {
-        
+        controls = new PlayerControls();
+        groundMovement = controls.GroundMovement;
+
+        // groundMovement.[action].performed += context => do something
+        groundMovement.HorizontalMovement.performed += ctx => horizontalInput = ctx.ReadValue<Vector2>();
+
+        groundMovement.Jump.performed += _ => movement.OnJumpPressed();
+
+        groundMovement.MouseX.performed += ctx => mouseInput.x = ctx.ReadValue<float>();
+        groundMovement.MouseY.performed += ctx => mouseInput.y = ctx.ReadValue<float>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update ()
     {
-        
+        movement.ReceiveInput(horizontalInput);
+        mouseLook.ReceiveInput(mouseInput);
+    }
+    
+
+    private void OnEnable ()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable ()
+    {
+        controls.Disable();
     }
 }
