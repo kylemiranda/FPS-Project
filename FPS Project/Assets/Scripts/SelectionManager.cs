7cc14187dc9c93b85c251008ca037cc96dc8ac61
Kyle Miranda <miranda14355k@gmail.com>
2021-08-20 12:27:23 -0400

    Item Selection
    Added simple item selection on where the player is looking. Also made the look sensitivity more fluid.

---
+++ FPS Project/Assets/Scripts/SelectionManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SelectionManager : MonoBehaviour
+{
+
+    [SerializeField] private string selectableTab = "Selectable";
+    [SerializeField] private Material selectedMaterial;
+    [SerializeField] private Material defaultMaterial;
+
+    private Transform _selection;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (_selection != null)
+        {
+            var selectionRenderer = _selection.GetComponent<Renderer>();
+            if (selectionRenderer != null)
+            {
+                selectionRenderer.material = defaultMaterial;   
+            }
+        }
+        
+        var ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        
+        _selection = null;
+        if (Physics.Raycast(ray, out var hit))
+        {
+            var selection = hit.transform;
+            if (selection.CompareTag(selectableTab))
+            {
+                _selection = selection;
+            }
+        }
+
+        if (_selection != null)
+        {
+            var selectionRenderer = _selection.GetComponent<Renderer>();
+            if (selectionRenderer != null)
+            {
+                selectionRenderer.material = selectedMaterial;
+            }
+        }
+    }
+}
